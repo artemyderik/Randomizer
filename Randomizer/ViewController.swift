d585c10d80bee00879cb56e9e305536923541f75
ArtemyDerik <derry0808@gmail.com>
2023-03-12 06:38:27 +0700

    GetOutlets

--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    
+    @IBOutlet var getRundomNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getRandomNumberButtonTapped() {
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 0
+        
+        randomValueLabel.text = Int.random(
+            in: minimumNumber...maximumNumber
+        ).formatted()
+    }
 }
 
